f6a40441956f7f858e88c6d95ba57be71ef37ddb
zkfy <lib4net@163.com>
2018-05-21 14:48:31 +0800

    添加功能逻辑

--- modules/sql/role_menu.sql
+++ modules/sql/role_menu.sql
@@ -1,10 +1,10 @@
 create table sso_role_menu_info(
 	id number(20) not null,
-  sys_id number(20) not null
+  sys_id number(20) not null,
   role_id number(20) not null,
   menu_id number(20) not null,
-  status number(1) default 0 not null
-  create_time date default sysdate not null
+  status number(1) default 0 not null,
+  create_time date default sysdate not null,
   sortrank number(20) not null
   );
 
@@ -19,20 +19,20 @@ comment on column sso_menu_info.sortrank is '排序编号';
 
 
 alter table sso_role_menu_info
-add constraint pk_menu_info primary key(id);
+add constraint pk_role_menu_info primary key(id);
 
 alter table sso_user_info
-add constraint pk_user_info unique(sys_id,role_id,menu_id);
+add constraint unq_role_menu_info unique(sys_id,role_id,menu_id);
 
 
-create sequence seq_menu_info_id
+create sequence seq_role_menu_id
 minvalue 10000
 maxvalue 99999
 start with 10000
 cache 20;
 
 
-create sequence seq_menu_info_sort_id
+create sequence seq_role_menu_sort_id
 minvalue 1
 maxvalue 9999
 start with 1
