7c5dc8db5ea278c22b8228ae794c47e0d789dd93
ikaros56 <lvcw@100bm.cn>
2018-07-18 18:51:51 +0800

    .

---
+++ modules/const/enum/enum.go
@@ -0,0 +1,10 @@
+package enum
+
+const UserDefaultPassword = `abc123$`
+
+const (
+	Normal   = 0
+	Locked   = 1
+	Disabled = 2
+	Unlock   = 11
+)
---
+++ services/user/user.query.go
@@ -0,0 +1,40 @@
+package user
+
+import (
+	"github.com/micro-plat/hydra/component"
+	"github.com/micro-plat/hydra/context"
+	"github.com/micro-plat/sso/modules/user"
+)
+
+type UserHandler struct {
+	container component.IContainer
+	userLib   user.IUser
+}
+
+func NewUserHandler(container component.IContainer) (u *UserHandler) {
+	return &UserHandler{
+		container: container,
+		userLib:   user.NewUser(container),
+	}
+}
+
+func (u *UserHandler) Handle(ctx *context.Context) (r interface{}) {
+
+	ctx.Log.Info("--------查询用户信息数据--------")
+	ctx.Log.Info("1.参数校验")
+	var inputData user.QueryUserInput
+	if err := ctx.Request.Bind(&inputData); err != nil {
+		return context.NewError(context.ERR_NOT_ACCEPTABLE, err)
+	}
+
+	rows, count, err := u.userLib.Query(inputData)
+	if err != nil {
+		return context.NewError(context.ERR_NOT_IMPLEMENTED, err)
+	}
+
+	ctx.Log.Info("2.返回数据。")
+	return map[string]interface{}{
+		"count": count.(string),
+		"list":  rows,
+	}
+}
