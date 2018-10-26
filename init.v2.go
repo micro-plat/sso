68f04b465f9b531c16ead480a248fd8a120a6050
zkfy <lib4net@163.com>
2018-10-26 15:51:29 +0800

    .

--- init.v2.go
+++ init.v2.go
@@ -45,33 +45,24 @@ func (r *SSO) init() {
 		return nil
 	})
 
-	r.Micro("/sso/login", member.NewLoginHandler, "*") //用户名密码登录,子系统模拟登录
-
-	r.Micro("/sso/ident", system.NewSystemIdentHandler, "*") //系统信息获取
-
-	r.Micro("/sso/menu", menu.NewMenuHandler, "*") //系统菜单相关接口
-
-	r.Micro("/sso/member", member.NewQueryHandler, "*") //查询登录用户信息
-
+	r.Micro("/sso/login", member.NewLoginHandler, "*")          //用户名密码登录,子系统模拟登录
+	r.Micro("/sso/ident", system.NewSystemIdentHandler, "*")    //系统信息获取
+	r.Micro("/sso/menu", menu.NewMenuHandler, "*")              //系统菜单相关接口
+	r.Micro("/sso/member", member.NewQueryHandler, "*")         //查询登录用户信息
 	r.Micro("/sso/changepwd", user.NewUserPasswordHandler, "*") // 修改密码
-
 	r.Micro("/sso/base", base.NewBaseUserHandler, "*")
 
-	r.Micro("/sso/user", user.NewUserHandler, "*") //用户相关接口
-
+	r.Micro("/sso/user", user.NewUserHandler, "*")              //用户相关接口
 	r.Micro("/sso/auth", role.NewRoleAuthHandler, "/user/role") //权限管理
-
-	r.Micro("/sso/role", role.NewRoleHandler, "/user/role") //角色管理相关接口
+	r.Micro("/sso/role", role.NewRoleHandler, "/user/role")     //角色管理相关接口
 
 	r.Micro("/sso/sys/manage", system.NewSystemHandler, "/sys/index#[post:addsys]") //系统管理相关接口
-
-	r.Micro("/sso/sys/func", function.NewSystemFuncHandler, "/sys/index") //系统功能相关接口
+	r.Micro("/sso/sys/func", function.NewSystemFuncHandler, "/sys/index")           //系统功能相关接口
 
 	r.Micro("/sso/img/upload", image.NewImageHandler("./static/static/img", "http://sso.sinopecscsy.com"), "*") //图片上传
 
 	r.Micro("/sso/notify/info", notify.NewNotifyHandler, "*")
 	r.Micro("/sso/notify/settings", notify.NewNotifySetHandler, "*")
-
 	r.CRON("/sso/notify/send", notify.NewNotifySendHandler, "*") // 发送消息
 
 }
