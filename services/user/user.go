8521675d70aafcd250d3d2673dd5d6f5911c9604
wule61 <wule61@live.com>
2018-11-20 16:25:49 +0800

    !!++

--- services/user/user.go
+++ services/user/user.go
@@ -195,21 +195,21 @@ func (u *UserHandler) EditHandle(ctx *context.Context) (r interface{}) {
 
 	ctx.Log.Info("--------编辑个人基本资料--------")
 	ctx.Log.Info("1.参数校验")
-	if err := ctx.Request.Check("username", "tel", "email"); err != nil {
+	if err := ctx.Request.Check("user_name", "mobile", "email"); err != nil {
 		return context.NewError(context.ERR_NOT_ACCEPTABLE, err)
 	}
 	ctx.Log.Info("2.执行操作")
-	if err := u.userLib.Edit(ctx.Request.GetString("username"), ctx.Request.GetString("tel"), ctx.Request.GetString("email")); err != nil {
+	if err := u.userLib.Edit(ctx.Request.GetString("user_name"), ctx.Request.GetString("mobile"), ctx.Request.GetString("email")); err != nil {
 		return context.NewError(context.ERR_NOT_IMPLEMENTED, err)
 	}
 	ctx.Log.Info("3.记录行为")
 	if err := u.op.UserOperate(
 		member.Query(ctx, u.container),
 		"编辑个人资料",
-		"username",
-		ctx.Request.GetInt("username"),
-		"tel",
-		ctx.Request.GetInt("tel"),
+		"user_name",
+		ctx.Request.GetString("user_name"),
+		"mobile",
+		ctx.Request.GetInt("mobile"),
 		"email",
 		ctx.Request.GetString("email"),
 	); err != nil {
