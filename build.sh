f7148c9568873da76d0e7577cd39827d765b934f
zhangxh <1034992601@qq.com>
2019-08-01 18:19:02 +0800

    写脚本

--- build.sh
+++ build.sh
@@ -1,35 +1,82 @@
 #!/bin/sh
 
+rm -rf ./out 
+
 echo "sso五个项目的打包脚本...."
 
-echo "请正确配置所有的config信息"
-
-echo "1:生成apiserver数据"
-
-#out_dir ="out"
-#if [ ! -d "$out_dir" ]; then
-#        mkdir $out_dir
-#fi
-
-apiserver_out_dir="$out/$apiserver"
-if [ ! -d "$apiserver_out_dir" ]; then
-        mkdir $apiserver_out_dir
-fi
-
-cd apiserver
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $apiserver_out_dir main.go
-if [ $? -ne 0 ]; then
-	echo "apiserver 项目编译出错,请检查"
+read -p "所有配置参数都改好了？确认请输入[yes],否则输入[no]: " flag
+temp=$(echo $flag | tr [A-Z] [a-z])
+if [ $temp != "yes" ]; then
+	echo "请前去修改"
 	exit
 fi
 
 
-echo "2:生成lgapi数据"
+echo "----------1:生成apiserver数据-----------"
+cd apiserver/
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "../out/apiserver/apiserver"
+if [ $? -ne 0 ]; then
+	echo "apiserver 项目编译出错,请检查"
+	exit
+fi
+cd ../
 
-echo "3:生成lgweb数据"
+echo "----------2:生成lgapi数据------------"
+cd lgapi/
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "../out/lg/bin/lgapi"
+if [ $? -ne 0 ]; then
+	echo "lgapi 项目编译出错,请检查"
+	exit
+fi
+cd ../
 
-echo "4:生成mgrapi数据"
+echo "----------3:生成lgweb数据-----------"
+cd lgweb/
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "../out/lg/web/lgweb"
+if [ $? -ne 0 ]; then
+	echo "lgweb golang 项目编译出错,请检查"
+	exit
+fi
 
-echo "5:生成mgrweb数据"
+rm -rf ./dist/
+echo ""
+echo "--------------------------------------"
+npm run build
+if [ $? -ne 0 ]; then
+	echo "lgweb vue项目编译出错,请检查"
+	exit
+fi
+cp -r ./dist/static/ ../out/lg/web/static/
+echo "--------------------------------------"
+cd ../
 
+
+echo "----------4:生成mgrapi数据----------"
+cd mgrapi/
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "../out/mgr/bin/mgrapi"
+if [ $? -ne 0 ]; then
+	echo "mgrapi 项目编译出错,请检查"
+	exit
+fi
+cd ../
+
+echo "----------5:生成mgrweb数据------------"
+cd mgrweb/
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "../out/mgr/web/mgrweb"
+if [ $? -ne 0 ]; then
+	echo "mgrweb golang 项目编译出错,请检查"
+	exit
+fi
+rm -rf ./dist/
+echo ""
+echo "--------------------------------------"
+npm run build
+if [ $? -ne 0 ]; then
+	echo "mgrweb vue项目编译出错,请检查"
+	exit
+fi
+cp -r ./dist/static/ ../out/mgr/web/static/
+echo "--------------------------------------"
+
+cd ../
 echo "生成数据完成"
