6da137b03c646b32e63b377612caee95cd1dfced
zhangxh <1034992601@qq.com>
2019-08-15 11:59:00 +0800

    fix bug

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 replace github.com/micro-plat/hydra => ../../../github.com/micro-plat/hydra
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20190424111038-f61b66f89f4a
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/golang/protobuf v1.3.1
 	github.com/golang/snappy v0.0.1
--- main/main.go
+++ main/main.go
@@ -1,6 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
-http: //192.168.5.78:6689
+	"github.com/micro-plat/sso/sso"
+)
+
+func main() {
+	client, err := sso.New("http://192.168.5.78:6689", "sso", "B128F779D5741E701923346F7FA9F95C")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	client.GetSystemInfo()
 }
