576b2251084eeeef3235a02697ec27500411328d
zkfy <lib4net@163.com>
2019-08-15 11:37:10 +0800

    修改签名

--- apiserver/handling.go
+++ apiserver/handling.go
@@ -18,8 +18,8 @@ func (r *SSO) handling() {
 			return err
 		}
 
-		if ok, raw := ctx.Request.CheckSign(secret); !ok {
-			return context.NewErrorf(context.ERR_PAYMENT_REQUIRED, "sign签名错误:%s", raw)
+		if ok, err := ctx.Request.CheckSign(secret); !ok {
+			return context.NewError(context.ERR_PAYMENT_REQUIRED, err)
 		}
 
 		// data := ctx.Request.GetRequestMap("utf8")
