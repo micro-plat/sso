7d38aa808f0da78877bd67ad8d1c2a68542df055
zkfy <lib4net@163.com>
2019-08-15 09:18:46 +0800

    修改项目引用

--- apiserver/install.dev.go
+++ apiserver/install.dev.go
@@ -2,28 +2,14 @@
 
 package main
 
+import "github.com/micro-plat/hydra/conf"
+
 //bindConf 绑定启动配置， 启动时检查注册中心配置是否存在，不存在则引导用户输入配置参数并自动创建到注册中心
 func (s *SSO) install() {
 	s.IsDebug = true
-
-	s.Conf.API.SetMainConf(`{"address":":6689"}`)
-	s.Conf.API.SetSubConf("header", `
-				{
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,PATCH,OPTIONS",
-					"Access-Control-Allow-Headers": "X-Requested-With,Content-Type",
-					"Access-Control-Allow-Credentials": "true"
-				}
-			`)
-
-	s.Conf.Plat.SetVarConf("db", "db", `{			
-		"provider":"mysql",
-		"connString":"root:rTo0CesHi2018Qx@tcp(192.168.0.36:3306)/sso?charset=utf8",
-		"max":8,
-		"maxOpen":20,
-	 	"maxIdle":10,
-	 	"lifeTime":600		
-	}`)
+	s.Conf.API.SetMain(conf.NewAPIServerConf(":6689"))
+	s.Conf.API.SetHeaders(conf.NewHeader().WithCrossDomain())
+	s.Conf.Plat.SetDB(conf.NewMysqlConf("root", "rTo0CesHi2018Qx", "192.168.0.36:3306", "sso"))
 
 	s.Conf.Plat.SetVarConf("cache", "cache", `
 		{
