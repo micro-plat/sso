bde799a51ec0c07b42e0a5108d75d9cbb4308ed0
zhangxh <1034992601@qq.com>
2019-08-16 10:46:54 +0800

    去除多余的

--- mgrapi/services/menu/menu.go
+++ mgrapi/services/menu/menu.go
@@ -1,8 +1,6 @@
 package menu
 
 import (
-	"strings"
-
 	"github.com/micro-plat/sso/mgrapi/modules/access/member"
 
 	"github.com/micro-plat/hydra/component"
@@ -30,43 +28,11 @@ func NewMenuHandler(container component.IContainer) (u *MenuHandler) {
 //GetHandle 调用远程api得到用户的菜单数据
 func (u *MenuHandler) GetHandle(ctx *context.Context) (r interface{}) {
 	user := member.Get(ctx)
-	//menus, err := u.m.QueryMenuFromSso(user.UserID, user.SystemID)
 	menus, err := model.GetSSOClient(u.c).GetUserMenu(int(user.UserID))
 	if err != nil {
 		return err
 	}
 	return menus
-}
-
-//PostHandle 查询常用菜单
-func (u *MenuHandler) PostHandle(ctx *context.Context) (r interface{}) {
-	uid := member.Get(ctx).UserID
-	sysid := member.Get(ctx).SystemID
-	data, err := u.m.Query(uid, sysid)
-	if err != nil {
-		return err
-	}
-	return data
-}
-
-//PutHandle 添加常用菜单
-func (u *MenuHandler) PutHandle(ctx *context.Context) (r interface{}) {
-	if err := ctx.Request.Check("menu_ids", "pids"); err != nil {
-		return err
-	}
-	menuIds := strings.Split(ctx.Request.GetString("menu_ids"), ",")
-	pids := strings.Split(ctx.Request.GetString("pids"), ",")
-	if len(menuIds) != len(pids) || len(menuIds) > 20 {
-		return context.NewError(context.ERR_NOT_ACCEPTABLE, "菜单个数错误")
-	}
-
-	uid := member.Get(ctx).UserID
-	sysid := member.Get(ctx).SystemID
-	err := u.p.Save(uid, sysid, pids, menuIds)
-	if err != nil {
-		return err
-	}
-	return "success"
 }
 
 //VerifyHandle 查询用户在指定系统的页面是否有权限
@@ -81,18 +47,3 @@ func (u *MenuHandler) VerifyHandle(ctx *context.Context) (r interface{}) {
 	}
 	return ""
 }
-
-/*
-//GetHandle 查询指定用户在指定系统的菜单列表
-func (u *MenuHandler) GetHandle(ctx *context.Context) (r interface{}) {
-	l := member.Query(ctx, u.c)
-	if l == nil {
-		return context.NewError(context.ERR_FORBIDDEN, "code not be null")
-	}
-	data, err := u.m.Query(l.UserID, l.SystemID)
-	if err != nil {
-		return err
-	}
-	return data
-}
-*/
