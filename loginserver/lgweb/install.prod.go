029793b8d244bdee5a7af51f1c6352fc72c72e67
zhangxh <1034992601@qq.com>
2020-02-05 11:08:34 +0800

    js sdk修改,支持外部系统跳转登录

--- loginserver/lgweb/install.prod.go
+++ loginserver/lgweb/install.prod.go
@@ -1,9 +1,12 @@
 package main
 
-import "github.com/micro-plat/hydra/conf"
-
 func (s *lgweb) install() {
 	s.IsDebug = false
-	s.Conf.WEB.SetMainConf(`{"address":":8091", "host":"login.sso.18jiayou1.com"}`)
-	s.Conf.WEB.SetStatic(conf.NewWebServerStaticConf().WithArchive("./static.zip"))
+	s.Conf.WEB.SetMainConf(`{"address":":8091"}`)
+	//s.Conf.WEB.SetStatic(conf.NewWebServerStaticConf().WithArchive("./static.zip"))
+	s.Conf.WEB.SetSubConf("static", `{
+		"dir":"./static",
+		"rewriters":["*"],
+		"exts":[".ttf",".woff",".woff2"]			
+	}`)
 }
--- mgrserver/mgrweb/install.prod.go
+++ mgrserver/mgrweb/install.prod.go
@@ -1,10 +1,13 @@
 package main
 
-import "github.com/micro-plat/hydra/conf"
-
 func (s *mgrweb) install() {
 	s.IsDebug = false
 	//s.Conf.WEB.SetMainConf(`{"address":":8083", "host":"web.sso.18jiayou1.com"}`)
-	s.Conf.WEB.SetMainConf(`{"address":":80", "host":"web.sso.18jiayou1.com"}`)
-	s.Conf.WEB.SetStatic(conf.NewWebServerStaticConf().WithArchive("./static.zip"))
+	s.Conf.WEB.SetMainConf(`{"address":":8083"}`)
+	//s.Conf.WEB.SetStatic(conf.NewWebServerStaticConf().WithArchive("./static.zip"))
+	s.Conf.WEB.SetSubConf("static", `{
+		"dir":"./static",
+		"rewriters":["*"],
+		"exts":[".ttf",".woff",".woff2"]			
+	}`)
 }
