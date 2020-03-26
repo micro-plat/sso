10e2fdbd307ea415cadbdf9b4fbb858f35592b9c
zhangxh <1034992601@qq.com>
2020-03-26 18:39:14 +0800

    修改配置

--- mgrserver/mgrweb/config/prod.env.hbs.js
+++ mgrserver/mgrweb/config/prod.env.hbs.js
@@ -1,15 +1,14 @@
 'use strict'
 module.exports = {
     NODE_ENV: '"production"',
-    //这个配置文件是内部sso使用
+    //这个配置文件是hbs_sso使用, 专门管理sass用户信息的
 
     //SSO_WEB_HOST:`"http://login.sso.18jiayou0.com:8091"`,
     //VUE_APP_API_URL:'"http://webapi.sso.18jiayou.com"', //内部用户系统用
     //VUE_APP_API_URL:'"http://api.bss.sso.17ebs.18jiayou0.com:6677"', //sass用户系统用 
     //VUE_APP_API_URL:'"http://api.bss.sso.17ebs.18jiayou.com"', //sass用户系统用 正式环境
     SSO_WEB_HOST:`"http://login.sso.18jiayou.com"`,
-    //VUE_APP_API_URL:'"http://api.bss.sso.hbs.18jiayou.com:6677"', //内部用户系统用
-    VUE_APP_API_URL:'"http://webapi.sso.18jiayou1.com:6677"',
-    //IDENT:`"hbs_sso"`,
-    IDENT:`"sso"`,
+    VUE_APP_API_URL:'"http://api.bss.sso.hbs.18jiayou.com:6677"', //内部用户系统用
+    //VUE_APP_API_URL:'"http://webapi.sso.18jiayou1.com:6677"',
+    IDENT:`"hbs_sso"`,
 }
