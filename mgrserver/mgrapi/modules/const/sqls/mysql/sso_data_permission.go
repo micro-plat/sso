915fe52433c655079dc5439b6f0ebcf02f6935cf
taoshouyin <527503855@qq.com>
2020-12-28 17:41:21 +0800

    修改sqlg

--- mgrserver/mgrapi/modules/const/sqls/mysql/sso_data_permission.go
+++ mgrserver/mgrapi/modules/const/sqls/mysql/sso_data_permission.go
@@ -1,13 +1,13 @@
 package mysql
 
 const sso_data_permission = `CREATE TABLE  sso_data_permission (
-		id number(20)  not null  comment 'id' ,
-		sys_id number(20)  not null  comment '系统编号' ,
-		ident varchar2(32)  not null  comment '系统标识' ,
-		name varchar2(128)  not null  comment '名称' ,
-		table_name varchar2(128)  not null  comment '表名' ,
-		operate_action varchar2(64)  not null  comment '操作动作' ,
+		id bigint(20)  not null  comment 'id' ,
+		sys_id bigint(20)  not null  comment '系统编号' ,
+		ident varchar(32)  not null  comment '系统标识' ,
+		name varchar(128)  not null  comment '名称' ,
+		table_name varchar(128)  not null  comment '表名' ,
+		operate_action varchar(64)  not null  comment '操作动作' ,
 		rules LONGTEXT    comment '规则json' ,
-		remark varchar2(256)  not null  comment '说明' ,
-		status number(4) default 0 not null  comment '状态 0: 启用 1: 禁用' ,
+		remark varchar(256)  not null  comment '说明' ,
+		status tinyint(4) default 0 not null  comment '状态 0: 启用 1: 禁用' ,
 		PRIMARY KEY (id)) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='数据权限规则表';`
--- mgrserver/mgrapi/modules/const/sqls/mysql/sso_role_datapermission.go
+++ mgrserver/mgrapi/modules/const/sqls/mysql/sso_role_datapermission.go
@@ -1,9 +1,9 @@
 package mysql
 
 const sso_role_datapermission = `CREATE TABLE  sso_role_datapermission (
-	id number(20)  not null  comment '功能编号' ,
-	sys_id number(20)  not null  comment '系统编号' ,
-	role_id number(20)  not null  comment '角色编号' ,
-	permission_config_id number(20)  not null  comment '规则id' ,
+	id bigint(20)  not null  comment '功能编号' ,
+	sys_id bigint(20)  not null  comment '系统编号' ,
+	role_id bigint(20)  not null  comment '角色编号' ,
+	permission_config_id bigint(20)  not null  comment '规则id' ,
 	create_time DATETIME default current_timestamp not null  comment '创建时间' ,
 	PRIMARY KEY (id)) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='角色与规则关联信息表';`
