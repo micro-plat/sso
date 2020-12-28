0e258f4aa257e0fcb1db0e97e9a1667951097121
taoshouyin <527503855@qq.com>
2020-12-28 19:20:44 +0800

    去掉日志

--- mgrserver/mgrapi/modules/const/sqls/mysql/install.go
+++ mgrserver/mgrapi/modules/const/sqls/mysql/install.go
@@ -1,8 +1,6 @@
 package mysql
 
 import (
-	"fmt"
-
 	"github.com/micro-plat/hydra"
 )
 
@@ -11,22 +9,6 @@ func init() {
 	names := AssetNames()
 	for i := range names {
 		bytes, _ := Asset(names[i])
-		fmt.Println(names[i], string(bytes))
 		hydra.Installer.DB.AddSQL(string(bytes))
 	}
-	/*
-		hydra.Installer.DB.AddSQL(
-			dds_area_info,
-			dds_dictionary_info,
-			sso_data_permission,
-			sso_operate_log,
-			sso_role_datapermission,
-			sso_role_info,
-			sso_role_menu,
-			sso_system_info,
-			sso_system_menu,
-			sso_user_info,
-			sso_user_role,
-		)
-	*/
 }
