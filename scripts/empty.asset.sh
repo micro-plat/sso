f4a9a4b957f6f516ee63f6f766767d06cc987018
zkfy <lib4net@163.com>
2021-01-05 16:00:19 +0800

    更新项目结构

--- scripts/empty.asset.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-filepath=$1
-echo "" > $filepath/static.go
-echo "package web" >> $filepath/static.go
-echo "import \"fmt\"" >> $filepath/static.go
-echo "func Asset(name string) ([]byte, error) { return nil, fmt.Errorf(\"Asset %s not found\", name) }" >> $filepath/static.go
-echo "func AssetNames() []string              { return []string{} }" >> $filepath/static.go
