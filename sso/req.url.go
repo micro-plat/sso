944ada69fc88fc71f3feb63e56dc0475bc885f5a
liujinyin <liujy@100bm.cn>
2021-01-19 17:46:58 +0800

    gengx

--- sso/req.url.go
+++ sso/req.url.go
@@ -1,15 +1,17 @@
 package sso
 
-const userInfoUrl = "/member/info/get"
-const codeLoginUrl = "/login/auth"
-const systemInfoUrl = "/system/info/get"
-const roleAllUser = "/role/user/get"
-const userMenuUrl = "/member/menu/get"
-const userSysUrl = "/member/system/get"
-const userAllUrl = "/member/all/get"
-const userAllTag = "/member/tags/get"
-const permissionConfig = "/permission/config/obtain"
-const addUser = "/user/add"                            //增加用户
-const passwordLogin = "/user/login"                    //用户名密码登录
-const changePassword = "/user/changepwd"               //修改密码
-const forgetPassword = "/member/forget/possord/change" //忘记密码并修改密码
+const userInfoUrl = "/api/user/info/get"
+const userMenuUrl = "/api/menu/get"
+const userSysUrl = "/api/user/system/list"
+const userAllUrl = "/api/all/get"
+const userAllTag = "/api/tags/get"
+
+const codeLoginUrl = "/api/login/auth"
+const systemInfoUrl = "/api/system/info/get"
+const roleAllUser = "/api/role/get"
+
+const permissionConfig = "/api/permission/config/obtain"
+const addUser = "/api/user/add"                      //增加用户
+const passwordLogin = "/api/user/login"              //用户名密码登录
+const changePassword = "/api/user/changepwd"         //修改密码
+const forgetPassword = "/api/forget/password/change" //忘记密码并修改密码
