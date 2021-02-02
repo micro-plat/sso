c052af54ec3b5c659c901da1951e72e7b7e1035c
liujinyin <liujy@100bm.cn>
2021-02-02 09:25:57 +0800

    修改url 地址

--- sso/req.url.go
+++ sso/req.url.go
@@ -3,7 +3,7 @@ package sso
 const userInfoUrl = "/api/user/info/get"
 const userMenuUrl = "/api/menu/get"
 const userSysUrl = "/api/user/system/list"
-const userAllUrl = "/api/all/get"
+const userAllUrl = "/api/user/all/get"
 const userAllTag = "/api/tags/get"
 const getRoleTags = "/api/role/tags"
 
@@ -16,3 +16,4 @@ const addUser = "/api/user/add"                      //增加用户
 const passwordLogin = "/api/user/login"              //用户名密码登录
 const changePassword = "/api/user/changepwd"         //修改密码
 const forgetPassword = "/api/forget/password/change" //忘记密码并修改密码
+
